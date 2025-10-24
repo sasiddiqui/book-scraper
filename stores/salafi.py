23dee295acea04f8c0c88fb718064a90f9bef187
Abdullah Mohammad <abdullahmohammad@MacBook-Pro-2.local>
2025-10-23 22:21:50 -0500

    fixed batch size

--- stores/salafi.py
+++ stores/salafi.py
@@ -5,7 +5,7 @@ import re
 class Salafi(AbstractBookScraper):
     def __init__(self):
         super().__init__("https://salafibookstore.com", "Salafi Books", convert_rate=1.33)
-        self.batch_size = 15
+        self.batch_size = 8
         self.test_urls = [
             "https://salafibookstore.com/product/taleeq-ala-meemiyyah-ibn-al-qayyim/",
             "https://salafibookstore.com/product/the-book-of-manners/",
